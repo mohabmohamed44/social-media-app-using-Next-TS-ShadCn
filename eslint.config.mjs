79488759a0443099c0da08363e88bf128f4cfa8c
mohabmohamed44 <mohamedmohab742@gmail.com>
2026-06-06 21:56:10 +0300

    chore: downgrade eslint and eslint-config-next to compatible versions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,9 @@ const eslintConfig = [
   {
     ignores: [".next/**", "out/**", "build/**"],
   },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+  }),
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
